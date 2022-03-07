55a2fd572468f01a1f41372274d5ca62845f3833
Cameron Torrance <cameron.d.torrance@outlook.com>
2022-03-07 13:39:28 +0000

    blah

--- src/algebra/comm_rings/classes.lean
+++ src/algebra/comm_rings/classes.lean
@@ -7,6 +7,11 @@ universes u v
 
 namespace comm_ring
 
+/-
+  In this file we define various important classes of commuative ring
+  to be used elsewhere.
+-/
+
 class local_ring (R : Type u) [comm_ring R] :=
   (unquie_maximal_ideal : MaxSpec R)
   (only_one_maximal_ideal : ∀ m : MaxSpec R, m = unquie_maximal_ideal)
